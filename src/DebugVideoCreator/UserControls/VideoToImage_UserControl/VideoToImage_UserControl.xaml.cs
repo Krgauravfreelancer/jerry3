a53f1d5a82bec183886a6e147680b7a4643f319e
Kumar Gaurav <krgourav007@gmail.com>
2023-11-28 21:09:24 +0530

    Added Empty Video to Image UC

---
+++ src/DebugVideoCreator/UserControls/VideoToImage_UserControl/VideoToImage_UserControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Sqllite_Library.Business;
+using Sqllite_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace VideoToImage_UserControl
+{
+    /// <summary>
+    /// Interaction logic for VideoToImage_UserControl.xaml
+    /// </summary>
+    public partial class VideoToImage_UserControl : UserControl
+    {
+        public VideoToImage_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
