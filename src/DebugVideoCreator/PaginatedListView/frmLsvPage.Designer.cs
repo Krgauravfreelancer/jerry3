a76f3786ae5717e691ae433bd2f99442b5aaae60
Kumar Gaurav <krgourav007@gmail.com>
2023-12-20 19:28:49 +0530

    Code Refactoring

--- src/DebugVideoCreator/PaginatedListView/frmLsvPage.Designer.cs
+++ src/DebugVideoCreator/PaginatedListView/frmLsvPage.Designer.cs
@@ -32,10 +32,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLsvPage));
             this.lsvData = new System.Windows.Forms.ListView();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.lblFrmDescription = new System.Windows.Forms.Label();
-            this.pnlTop = new System.Windows.Forms.Panel();
-            this.lblFrmTitle = new System.Windows.Forms.Label();
-            this.picTop = new System.Windows.Forms.PictureBox();
             this.pnlNavigate = new System.Windows.Forms.Panel();
             this.btnFirst = new System.Windows.Forms.Button();
             this.btnPrev = new System.Windows.Forms.Button();
@@ -45,8 +41,6 @@
             this.pnlNRPP = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.nudNRPP = new System.Windows.Forms.NumericUpDown();
-            this.pnlTop.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picTop)).BeginInit();
             this.pnlNavigate.SuspendLayout();
             this.pnlNRPP.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudNRPP)).BeginInit();
@@ -57,9 +51,9 @@
             this.lsvData.FullRowSelect = true;
             this.lsvData.GridLines = true;
             this.lsvData.HideSelection = false;
-            this.lsvData.Location = new System.Drawing.Point(8, 96);
+            this.lsvData.Location = new System.Drawing.Point(8, 50);
             this.lsvData.Name = "lsvData";
-            this.lsvData.Size = new System.Drawing.Size(408, 182);
+            this.lsvData.Size = new System.Drawing.Size(554, 262);
             this.lsvData.SmallImageList = this.imageList1;
             this.lsvData.TabIndex = 0;
             this.lsvData.UseCompatibleStateImageBehavior = false;
@@ -71,48 +65,6 @@
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             this.imageList1.Images.SetKeyName(0, "plus_32.png");
             // 
-            // lblFrmDescription
-            // 
-            this.lblFrmDescription.AutoSize = true;
-            this.lblFrmDescription.Location = new System.Drawing.Point(51, 31);
-            this.lblFrmDescription.Name = "lblFrmDescription";
-            this.lblFrmDescription.Size = new System.Drawing.Size(197, 13);
-            this.lblFrmDescription.TabIndex = 8;
-            this.lblFrmDescription.Text = "A Simple Approach to Paging in Listview";
-            // 
-            // pnlTop
-            // 
-            this.pnlTop.BackColor = System.Drawing.Color.White;
-            this.pnlTop.Controls.Add(this.lblFrmDescription);
-            this.pnlTop.Controls.Add(this.lblFrmTitle);
-            this.pnlTop.Controls.Add(this.picTop);
-            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlTop.Location = new System.Drawing.Point(0, 0);
-            this.pnlTop.Name = "pnlTop";
-            this.pnlTop.Size = new System.Drawing.Size(574, 48);
-            this.pnlTop.TabIndex = 20;
-            // 
-            // lblFrmTitle
-            // 
-            this.lblFrmTitle.AutoSize = true;
-            this.lblFrmTitle.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFrmTitle.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblFrmTitle.Location = new System.Drawing.Point(51, 4);
-            this.lblFrmTitle.Name = "lblFrmTitle";
-            this.lblFrmTitle.Size = new System.Drawing.Size(221, 23);
-            this.lblFrmTitle.TabIndex = 9;
-            this.lblFrmTitle.Text = "List View With Paging";
-            // 
-            // picTop
-            // 
-            this.picTop.Image = ((System.Drawing.Image)(resources.GetObject("picTop.Image")));
-            this.picTop.Location = new System.Drawing.Point(0, 0);
-            this.picTop.Name = "picTop";
-            this.picTop.Size = new System.Drawing.Size(48, 48);
-            this.picTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picTop.TabIndex = 8;
-            this.picTop.TabStop = false;
-            // 
             // pnlNavigate
             // 
             this.pnlNavigate.BackColor = System.Drawing.Color.White;
@@ -122,9 +74,10 @@
             this.pnlNavigate.Controls.Add(this.btnLast);
             this.pnlNavigate.Controls.Add(this.lblInfo);
             this.pnlNavigate.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlNavigate.Location = new System.Drawing.Point(0, 327);
+            this.pnlNavigate.Location = new System.Drawing.Point(0, 314);
             this.pnlNavigate.Name = "pnlNavigate";
-            this.pnlNavigate.Size = new System.Drawing.Size(574, 32);
+            this.pnlNavigate.Padding = new System.Windows.Forms.Padding(5);
+            this.pnlNavigate.Size = new System.Drawing.Size(574, 45);
             this.pnlNavigate.TabIndex = 22;
             // 
             // btnFirst
@@ -132,9 +85,9 @@
             this.btnFirst.BackgroundImage = global::DebugVideoCreator.Properties.Resources.first;
             this.btnFirst.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnFirst.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnFirst.Location = new System.Drawing.Point(446, 0);
+            this.btnFirst.Location = new System.Drawing.Point(441, 5);
             this.btnFirst.Name = "btnFirst";
-            this.btnFirst.Size = new System.Drawing.Size(32, 32);
+            this.btnFirst.Size = new System.Drawing.Size(32, 35);
             this.btnFirst.TabIndex = 20;
             this.btnFirst.UseVisualStyleBackColor = true;
             this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
@@ -144,9 +97,9 @@
             this.btnPrev.BackgroundImage = global::DebugVideoCreator.Properties.Resources.previous;
             this.btnPrev.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnPrev.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnPrev.Location = new System.Drawing.Point(478, 0);
+            this.btnPrev.Location = new System.Drawing.Point(473, 5);
             this.btnPrev.Name = "btnPrev";
-            this.btnPrev.Size = new System.Drawing.Size(32, 32);
+            this.btnPrev.Size = new System.Drawing.Size(32, 35);
             this.btnPrev.TabIndex = 21;
             this.btnPrev.UseVisualStyleBackColor = true;
             this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
@@ -156,9 +109,9 @@
             this.btnNext.BackgroundImage = global::DebugVideoCreator.Properties.Resources.next;
             this.btnNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnNext.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnNext.Location = new System.Drawing.Point(510, 0);
+            this.btnNext.Location = new System.Drawing.Point(505, 5);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(32, 32);
+            this.btnNext.Size = new System.Drawing.Size(32, 35);
             this.btnNext.TabIndex = 22;
             this.btnNext.UseVisualStyleBackColor = true;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
@@ -168,9 +121,9 @@
             this.btnLast.BackgroundImage = global::DebugVideoCreator.Properties.Resources.last;
             this.btnLast.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnLast.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnLast.Location = new System.Drawing.Point(542, 0);
+            this.btnLast.Location = new System.Drawing.Point(537, 5);
             this.btnLast.Name = "btnLast";
-            this.btnLast.Size = new System.Drawing.Size(32, 32);
+            this.btnLast.Size = new System.Drawing.Size(32, 35);
             this.btnLast.TabIndex = 23;
             this.btnLast.UseVisualStyleBackColor = true;
             this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
@@ -180,7 +133,7 @@
             this.lblInfo.AutoSize = true;
             this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblInfo.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblInfo.Location = new System.Drawing.Point(8, 8);
+            this.lblInfo.Location = new System.Drawing.Point(13, 13);
             this.lblInfo.Name = "lblInfo";
             this.lblInfo.Size = new System.Drawing.Size(47, 15);
             this.lblInfo.TabIndex = 24;
@@ -190,7 +143,7 @@
             // 
             this.pnlNRPP.Controls.Add(this.label1);
             this.pnlNRPP.Controls.Add(this.nudNRPP);
-            this.pnlNRPP.Location = new System.Drawing.Point(224, 56);
+            this.pnlNRPP.Location = new System.Drawing.Point(8, 10);
             this.pnlNRPP.Name = "pnlNRPP";
             this.pnlNRPP.Size = new System.Drawing.Size(192, 32);
             this.pnlNRPP.TabIndex = 24;
@@ -229,16 +182,12 @@
             this.ClientSize = new System.Drawing.Size(574, 359);
             this.Controls.Add(this.pnlNRPP);
             this.Controls.Add(this.pnlNavigate);
-            this.Controls.Add(this.pnlTop);
             this.Controls.Add(this.lsvData);
             this.Name = "frmLsvPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Listview With Paging";
+            this.Text = "Media Library";
             this.Load += new System.EventHandler(this.frmLsvPage_Load);
             this.Resize += new System.EventHandler(this.frmLsvPage_Resize);
-            this.pnlTop.ResumeLayout(false);
-            this.pnlTop.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picTop)).EndInit();
             this.pnlNavigate.ResumeLayout(false);
             this.pnlNavigate.PerformLayout();
             this.pnlNRPP.ResumeLayout(false);
@@ -251,10 +200,6 @@
         #endregion
 
         private System.Windows.Forms.ListView lsvData;
-        private System.Windows.Forms.Label lblFrmDescription;
-        private System.Windows.Forms.Panel pnlTop;
-        private System.Windows.Forms.Label lblFrmTitle;
-        private System.Windows.Forms.PictureBox picTop;
         private System.Windows.Forms.Panel pnlNavigate;
         private System.Windows.Forms.Button btnFirst;
         private System.Windows.Forms.Button btnPrev;
--- src/DebugVideoCreator/PaginatedListView/frmLsvPage.cs
+++ src/DebugVideoCreator/PaginatedListView/frmLsvPage.cs
@@ -7,244 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.OleDb;
-
+using DebugVideoCreator.PaginatedListView;
 
 namespace VideoCreator.PaginatedListView
 {
-    
+
     public partial class frmLsvPage : Form
     {
-       public frmLsvPage()
+        public frmLsvPage()
         {
             InitializeComponent();
-            
         }
 
-       private void frmLsvPage_Load(object sender, EventArgs e)
-       {
-           lsvData.Width = this.Width - 30;
-           lsvData.Height = pnlNavigate.Top-100;
-           pnlNRPP.Left = this.Width - pnlNRPP.Width-30;
-           LsvPageGlobVar.NRPP = Convert.ToInt32(nudNRPP.Value);
-           
-           LsvPageFunc.DbConnection();
-           LsvPageGlobVar.Page = 1;
-           LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"),lsvData,0);
-           lblInfo.Text="Record Shown: " + LsvPageGlobVar.RecStart + " to "+LsvPageGlobVar.RecEnd+ "                      Page "+LsvPageGlobVar.Page+" of " + LsvPageGlobVar.TotalPages;
-       }
-
-       private void frmLsvPage_Resize(object sender, EventArgs e)
-       {
-           lsvData.Width = this.Width - 30;
-           lsvData.Height = pnlNavigate.Top - 100;
-           pnlNRPP.Left = this.Width - pnlNRPP.Width - 30;
-       }
-
-       private void btnFirst_Click(object sender, EventArgs e)
-       {
-           LsvPageGlobVar.Page = 1;
-           LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
-           lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
-       }
-
-       private void btnLast_Click(object sender, EventArgs e)
-       {
-           LsvPageGlobVar.Page = LsvPageGlobVar.TotalPages;
-           LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
-           lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
-       }
-
-       private void btnNext_Click(object sender, EventArgs e)
-       {
-           if (LsvPageGlobVar.Page < LsvPageGlobVar.TotalPages)
-           {
-               LsvPageGlobVar.Page++;
-           }
-           LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
-           lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
-       }
-
-       private void btnPrev_Click(object sender, EventArgs e)
-       {
-           if (LsvPageGlobVar.Page > 1)
-           {
-               LsvPageGlobVar.Page--;
-           }
-           LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
-           lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
-       }
-
-       private void nudNRPP_ValueChanged(object sender, EventArgs e)
-       {
-           if (nudNRPP.Value != 0)
-           {
-               LsvPageGlobVar.NRPP = Convert.ToInt32(nudNRPP.Value);
-           }
-           else
-           {
-               nudNRPP.Value = 1;
-           }
-           LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
-           lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
-           
-       }
-    }
-
-    public class LsvPageGlobVar
-    {
-        public static string ConStr;
-        public static DataTable sqlDataTable = new DataTable();
-        public static int TotalRec; //Variable for getting Total Records of the Table
-        public static int NRPP; //Variable for Setting the Number of Recrods per listiview page
-        public static int Page; //List View Page for Navigate or move
-        public static int TotalPages; //Varibale for Counting Total Pages.
-        public static int RecStart; //Variable for Getting Every Page Starting Record Index
-        public static int RecEnd; //Variable for Getting Every Page End Record Index
-    }
-    public class LsvPageFunc
-    {
-        public static bool DbConnection()
+        private void frmLsvPage_Load(object sender, EventArgs e)
         {
-            bool functionReturnValue = false;
+            lsvData.Width = this.Width - 30;
+            lsvData.Height = pnlNavigate.Top - 100;
+            pnlNRPP.Left = this.Width - pnlNRPP.Width - 30;
+            LsvPageGlobVar.NRPP = Convert.ToInt32(nudNRPP.Value);
 
-            try
-            {
-                 LsvPageGlobVar.ConStr = "Provider=Microsoft.Jet.OLEDB.4.0;Persist Security Info=False;Data Source=./PaginatedListView/Database.mdb";
-                //LsvPageGlobVar.ConStr ="Provider=Microsoft.Jet.OLEDB.4.0;Persist Security Info=False;Data Source=|DataDirectory|\data.mdb";      
-                
-                OleDbConnection sqlCon = new OleDbConnection();
-                sqlCon.ConnectionString = LsvPageGlobVar.ConStr;
-                sqlCon.Open();
-                functionReturnValue = true;
-                sqlCon.Close();
-            }
-            catch (Exception ex)
-            {
-                functionReturnValue = false;
-                MessageBox.Show("Error : " + ex.ToString());
-            }
-            return functionReturnValue;
+            LsvPageFunc.DbConnection();
+            LsvPageGlobVar.Page = 1;
+            LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
+            lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
         }
 
-        //Function to execute all queires
-        public static DataTable ExecSQLQry(string SQLQuery)
+        private void frmLsvPage_Resize(object sender, EventArgs e)
         {
-            try
-            {
-                OleDbConnection sqlCon = new OleDbConnection(LsvPageGlobVar.ConStr);
-                OleDbDataAdapter sqlDA = new OleDbDataAdapter(SQLQuery, sqlCon);
-                OleDbCommandBuilder sqlCB = new OleDbCommandBuilder(sqlDA);
-
-                LsvPageGlobVar.sqlDataTable.Reset();
-                sqlDA.Fill(LsvPageGlobVar.sqlDataTable);
-            }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show("Error : " + ex.ToString());
-
-            }
-            return LsvPageGlobVar.sqlDataTable;
+            lsvData.Width = this.Width - 30;
+            lsvData.Height = pnlNavigate.Top - 100;
+            pnlNRPP.Left = this.Width - pnlNRPP.Width - 30;
         }
 
-
-        public static void FillLsvData(DataTable sqlData, ListView lvList, int imageID)
+        private void btnFirst_Click(object sender, EventArgs e)
         {
-            //Load the table data in the listview
-            int i = 0;
-            int j = 0;
-            int m = 0;
-            int xsize;
-            
+            LsvPageGlobVar.Page = 1;
+            LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
+            lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
+        }
 
-            lvList.Clear();
-            // for Adding Column Names from the datatable
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            LsvPageGlobVar.Page = LsvPageGlobVar.TotalPages;
+            LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
+            lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
+        }
 
-            LsvPageGlobVar.TotalRec = sqlData.Rows.Count;
-
-            //try
-            //{
-                LsvPageGlobVar.TotalPages = LsvPageGlobVar.TotalRec / LsvPageGlobVar.NRPP;
-
-                if (LsvPageGlobVar.TotalRec % LsvPageGlobVar.NRPP > 0)
-                {
-                    LsvPageGlobVar.TotalPages++;
-                }
-            //}
-
-            //catch(DivideByZeroException e)
-            //{
-            //    MessageBox.Show("Error : " + e.ToString());
-            //}
-            
-
-            for (i = 0; i <= sqlData.Columns.Count - 1; i++)
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (LsvPageGlobVar.Page < LsvPageGlobVar.TotalPages)
             {
-                lvList.Columns.Add(sqlData.Columns[i].ColumnName);
+                LsvPageGlobVar.Page++;
             }
+            LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
+            lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
+        }
 
-            //for adding records to the listview from datatable
-            int l, k;
-            
-            l = (LsvPageGlobVar.Page - 1) * LsvPageGlobVar.NRPP;
-            k = ((LsvPageGlobVar.Page) * LsvPageGlobVar.NRPP);
-            
-            LsvPageGlobVar.RecStart = l + 1;
-            if (k > LsvPageGlobVar.TotalRec)
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (LsvPageGlobVar.Page > 1)
             {
-                LsvPageGlobVar.RecEnd = LsvPageGlobVar.TotalRec;
+                LsvPageGlobVar.Page--;
+            }
+            LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
+            lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
+        }
+
+        private void nudNRPP_ValueChanged(object sender, EventArgs e)
+        {
+            if (nudNRPP.Value != 0)
+            {
+                LsvPageGlobVar.NRPP = Convert.ToInt32(nudNRPP.Value);
             }
             else
             {
-                LsvPageGlobVar.RecEnd = k;
+                nudNRPP.Value = 1;
             }
+            LsvPageFunc.FillLsvData(LsvPageFunc.ExecSQLQry("Select * from tbl_Employee"), lsvData, 0);
+            lblInfo.Text = "Record Shown: " + LsvPageGlobVar.RecStart + " to " + LsvPageGlobVar.RecEnd + "                      Page " + LsvPageGlobVar.Page + " of " + LsvPageGlobVar.TotalPages;
 
-            for (; l < k; l++)
-            {
-                if (l >= LsvPageGlobVar.TotalRec)
-                {
-                    break;
-                }
-
-                lvList.Items.Add(sqlData.Rows[l][0].ToString(), imageID);
-               
-                for (j = 1; j <= sqlData.Columns.Count - 1; j++)
-                {
-                    if (!System.Convert.IsDBNull(sqlData.Rows[l][j]))
-                    {
-                        lvList.Items[m].SubItems.Add(sqlData.Rows[l][j].ToString());
-                        
-                    }
-                    else
-                    {
-                        lvList.Items[m].SubItems.Add("");
-                        
-                    } 
-                }
-                m++;
-            }
-
-
-            //for rearrange the column size
-            for (i = 0; i <= sqlData.Columns.Count - 1; i++)
-            {
-                xsize = lvList.Width / sqlData.Columns.Count - 8;
-
-                if (xsize > 1450)
-                {
-                    lvList.Columns[i].Width = xsize;
-                    lvList.Columns[i].AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
-                }
-
-                else
-                {
-                    lvList.Columns[i].Width = 2000;
-                    lvList.Columns[i].AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
-                }
-
-
-
-            }
         }
     }
+
+
+
 }
